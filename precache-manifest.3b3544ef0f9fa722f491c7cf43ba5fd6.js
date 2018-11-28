71b88f745f1c37d4b353a281725dafff30b558a0
Mickey L <mickey@MacBook-Pro.local>
2018-11-27 23:01:50 -0800

    Updates

---
+++ precache-manifest.3b3544ef0f9fa722f491c7cf43ba5fd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2090f3fb7ad3d8a6118bc4922db37581",
+    "url": "/stage-0-topic-proposal-3ickey/static/media/homebgResponsive.2090f3fb.svg"
+  },
+  {
+    "revision": "1624c7a4c10b5f41fe37c0c737851144",
+    "url": "/stage-0-topic-proposal-3ickey/static/media/HamMenu.1624c7a4.svg"
+  },
+  {
+    "revision": "0a83b9cbb8b06fce5cd2",
+    "url": "/stage-0-topic-proposal-3ickey/static/js/runtime~main.0a83b9cb.js"
+  },
+  {
+    "revision": "d40dde2cf79b6a08f8c6",
+    "url": "/stage-0-topic-proposal-3ickey/static/js/main.d40dde2c.chunk.js"
+  },
+  {
+    "revision": "ece2a95d44c1a26584dd",
+    "url": "/stage-0-topic-proposal-3ickey/static/js/1.ece2a95d.chunk.js"
+  },
+  {
+    "revision": "d40dde2cf79b6a08f8c6",
+    "url": "/stage-0-topic-proposal-3ickey/static/css/main.70ee8219.chunk.css"
+  },
+  {
+    "revision": "9fe943f5a361f8a5d726a4dde9c365eb",
+    "url": "/stage-0-topic-proposal-3ickey/index.html"
+  }
+];
