3f7fdfbd09c3584d72acf671c7d5636683bc4a00
tylervanbrocklin <tylerva@uw.edu>
2018-12-06 12:43:06 -0800

    fixed favorite loading issue

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -3,29 +3,47 @@ import SchoolCard from "./SchoolCard"
 import firebase from "firebase/app"
 
 class Favorites extends Component {
-    render() {
-        let favorites = null;
-        let heading = null;
-        if (this.props.currentUser) {
-            firebase.database().ref('users/' + this.props.currentUser.uid + '/favorites/').on('value', (snapshot) => {
-                let values = snapshot.val();
-                if(values != null) {
-                    favorites = (Object.keys(values).map(key => (
-                        <SchoolCard key={key} id={key} isFavorited={true} Schooldetails={values[key]} currentUser={this.props.currentUser}/>
-                    )));
-                } else {
-                    favorites = (<h3 className= "p-4"><em>Add schools to favorites and find them here!</em></h3>)
-                }
-            });
-             heading = (<h1 className= " searchTextPH p-4">Your favorite schools</h1>)
-        } else {
-            favorites = <h1 className="p-5">You must be logged in to use this feature!</h1>
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            favSchools: null
         }
+
+        this.changeState = this.changeState.bind(this);
+    }
+
+    changeState(data) {
+        this.setState({favSchools: data})
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                firebase.database().ref('users/' + user.uid + '/favorites/').on('value', (snapshot) => {
+                    let values = snapshot.val();
+                    let favorites = null;
+                    if(values != null) {
+                        favorites = (Object.keys(values).map(key => (
+                            <SchoolCard key={key} id={key} isFavorited={true} Schooldetails={values[key]} currentUser={user}/>
+                        )));
+                    } else {
+                        favorites = (<h3 className= "p-4"><em>Add schools to favorites and find them here!</em></h3>)
+                    }
+                    this.changeState(favorites);
+                });
+            } else {
+                this.setState({favSchools: (<h1 className="p-5">You must be logged in to use this feature!</h1>)})
+            }
+        });
+    }
+
+    render() {
         return (
             <div className="container pt-5">
-                {heading}
+                <h1 className= " searchTextPH p-4">Your favorite schools</h1>
                 <div className = "row">
-                    {favorites}
+                    {this.state.favSchools}
                 </div>
 
             </div>
